488ac4b536f63266395123ac7ae88e925465bd9a
unknown <pangeriz@outlook.com>
2020-06-12 20:38:24 +0100

    Updates

---
+++ precache-manifest.800883c8d6be1fd4ae4dfc9cae78a164.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08ae3a3988ef2dad2303f9dfb2c59fea",
+    "url": "/react-milestone-ii/index.html"
+  },
+  {
+    "revision": "57f22d447f4f4a9b9d0b",
+    "url": "/react-milestone-ii/static/css/main.2a5f4ab0.chunk.css"
+  },
+  {
+    "revision": "16c7d7373a72d022856b",
+    "url": "/react-milestone-ii/static/js/2.922e369a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-milestone-ii/static/js/2.922e369a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57f22d447f4f4a9b9d0b",
+    "url": "/react-milestone-ii/static/js/main.7e85d65f.chunk.js"
+  },
+  {
+    "revision": "2596cb31fe9340b5c027",
+    "url": "/react-milestone-ii/static/js/runtime-main.46fb0709.js"
+  },
+  {
+    "revision": "5414f47a6299a2ccd271a1f931bcf4db",
+    "url": "/react-milestone-ii/static/media/cake.5414f47a.jpg"
+  }
+]);
